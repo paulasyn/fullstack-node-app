2dddbe38533069878d664ba9159676e3400d79ad
Paul Asyn <paulasyn@gmail.com>
2019-01-22 09:39:37 -0600

    added express server for api backend

---
+++ api/server.js
@@ -0,0 +1,13 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.listen(PORT, function(){
+    console.log('Server is running on Port:', PORT);
+});
