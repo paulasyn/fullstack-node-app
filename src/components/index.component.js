1e717c68ca500ad8cfe895ec5b82c6fe32012955
Paul Asyn <paulasyn@gmail.com>
2019-01-28 09:19:49 -0600

    added index to view data

--- src/components/index.component.js
+++ src/components/index.component.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
 
 export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {business: [] };
+        }
+        componentDidMount(){
+            axios.get('http://localhost:4000/business')
+                .then(response => {
+                    this.setState({ business: response.data }):
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+
+        TableRow(){
+            return this.state.business.map(function(object, i){
+                return <TableRow obj={object} key={i} />;
+            });
+        }
+    
     render() {
         return (
             <div>
-                <p> Welcome to Index Component! </p>
+                <h3 align="center">Business List</h3>
+                <table className="table tabble-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>Person</th>
+                            <th>Busimess</th>
+                            <th>GST Number</th>
+                            <th colsSpan="2">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.tabRow() }
+                    </tbody>
+                </table>
             </div>
-        )
+        );
     }
 }
